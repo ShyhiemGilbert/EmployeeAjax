45302f9760407d6d8e8904e06136e49e2598a1ee
Shyhiem Gilbert <shyhiemg@icloud.com>
2019-01-24 14:35:36 +0000

    Configure View All Page

--- LiverpoolAjax/Controllers/EmployeeController.cs
+++ LiverpoolAjax/Controllers/EmployeeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LiverpoolAjax.Models;
+
 
 namespace LiverpoolAjax.Controllers
 {
@@ -13,5 +15,19 @@ namespace LiverpoolAjax.Controllers
         {
             return View();
         }
+
+		public ActionResult ViewAll()
+		{
+			return View(GetAllEmployee());
+		}
+
+		IEnumerable<EmployeeTbl> GetAllEmployee()
+		{
+			using (EmployeesEntities db = new EmployeesEntities())
+			{
+				return db.EmployeeTbls.ToList<EmployeeTbl>();
+			}
+
+		}
     }
 }
